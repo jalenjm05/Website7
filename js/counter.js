8b3ec35fff24778089903daa67078c084b78e032
Jalen Morales <112732395+Jsfordays30@users.noreply.github.com>
2023-03-21 21:48:18 +0000

    Tue, Mar 21, 2023, 2:48 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -1,10 +1,11 @@
 const counters = document.querySelectorAll('.counter')
+
 counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
-        const target = counter.getAttribute('data-target')
-        console.log(target)
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
     }
 
     updateCounter()

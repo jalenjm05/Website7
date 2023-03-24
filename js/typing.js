edd9f272a3bcbef2ddcce1369f2156e41fbc8011
Jalen Morales <112732395+Jsfordays30@users.noreply.github.com>
2023-03-24 21:57:17 +0000

    Fri, Mar 24, 2023, 2:57 PM -07:00

---
+++ js/typing.js
@@ -0,0 +1,12 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = "We love Programming!!!"
+let idx = 1
+let speed = 300
+
+writeText
+
+
+function writeText(){
+    textEl.innerText = text.slice(0,1)
+}
